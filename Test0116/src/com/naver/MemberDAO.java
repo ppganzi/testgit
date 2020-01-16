7606ebe9864663bccd2428d2fc1a7fea3628875c
ppganzi <lolo18@naver.com>
2020-01-16 17:03:14 +0900

    add 0116

---
+++ Test0116/src/com/naver/MemberDAO.java
@@ -0,0 +1,86 @@
+package com.naver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+	private static final String DRIVER = "oracle.jdbc.OracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static final String USER = "ca2";
+	private static final String PASSWORD = "ca2";
+	
+	public MemberDAO() {
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void closeAll(ResultSet rs,PreparedStatement pstmt,Connection conn) {
+		try {
+			if(rs != null) rs.close();
+			if(pstmt != null) pstmt.close();
+			if(conn != null) conn.close();
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	}
+	
+	public void insert(MemberDTO dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "insert into member2(id, name, age) values (?,?,?)";
+		try {
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("커넥션 생성");
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getName());
+			pstmt.setInt(3, dto.getAge());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			closeAll(null, pstmt, conn);
+		}
+	}
+
+	public List<MemberDTO> list() {
+		List<MemberDTO> list = new ArrayList<MemberDTO>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "select * from member2";
+		ResultSet rs =null;
+		
+		try {
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			pstmt = conn.prepareStatement(sql);
+			rs= pstmt.executeQuery();
+			
+			while (rs.next()) {
+				String id = rs.getString("id");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				
+				MemberDTO dto = new MemberDTO(id, name, age);
+				list.add(dto);
+				
+				
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			closeAll(rs, pstmt, conn);
+		}
+		
+		return list;
+	}
+	
+}
